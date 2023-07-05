5b79d179d8c6ae2c5d542c6dba0813c3a8d0caf6
Yea Ling Tay <yetay@u82z03s02.42kl.edu.my>
2023-07-05 15:13:24 +0800

    Rewrite to run printf and ft_printf in one single invocation (so that %p can print identical addresses). Also norminette check.

--- ft_printf_test.h
+++ ft_printf_test.h
@@ -1,8 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_test.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yetay <yetay@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 15:12:22 by yetay             #+#    #+#             */
+/*   Updated: 2023/07/05 15:12:30 by yetay            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_TEST_H
 # define FT_PRINTF_TEST_H
 
-#include <fcntl.h>
-#include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
 
 void	yt_putnbr_fd(int n, int fd);
 void	yt_putnbrline_fd(int i, int fd);
